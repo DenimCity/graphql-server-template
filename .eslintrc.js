3839c9888b9046d1fe51e2d9ce3ae3a862aed985
jxa8hst <jean_altidor@homdepot.com>
2019-07-13 02:40:07 -0400

    added linting rules

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "no-global-assign": "off",
+        "indent": [
+            "error",
+            4
+        ],
+        "no-undef": "off",
+        "no-no-global-assign": "off",
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
