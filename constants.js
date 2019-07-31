00cba9c5f1f63d84fbf051d693bbaedafdff8979
jxa8hst <jean_altidor@homdepot.com>
2019-07-31 19:39:57 -0400

    deletd constants

--- constants.js
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line import/prefer-default-export
-module.exports = {
-    getLaunch: 'https://api.spacexdata.com/v3/launches',
-    getLaunches: 'https://api.spacexdata.com/v3/launches',
-    getRockets: 'https://api.spacexdata.com/v3/rockets',
-};
