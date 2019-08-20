42cd87901b359783b998ff1c5d69d771e0e0baf1
jxa8hst <jean_altidor@homdepot.com>
2019-08-20 08:53:37 -0400

    resolver setup

--- src/graphql/resolvers/launches.js
+++ src/graphql/resolvers/launches.js
@@ -1,44 +1,50 @@
-
 export default {
-    Query: {
-        launches: async (root, args, { logger, request }) => new Promise((resolve, reject) => {
-            request({
-                uri: 'https://api.spacexdata.com/v3/launches',
-                strictSSL: false,
-                json: true,
-            }, (error, response, body) => {
-                if (error){
-                    logger.warn(getLaunch.name, `Error retrieving data ${error.message}`);
-                    reject(Error(error.message));
-                }
-                if (body){
-                    let results = body;
-                    if(!Array.from(body)){
-                        results = [body];
-                    }
-                    resolve(results);
-                }
-            });
-        
-        }),
-        launch: async (root, { flight_number }, { logger, request }) => new Promise((resolve, reject) =>{
-            request({
-                uri: `https://api.spacexdata.com/v3/launches/${flight_number}`,
-                strictSSL: false,
-                json: true,
-            }, (error, response, body) => {
-                if (error){
-                    logger.warn(getLaunch.name, `Error retrieving data ${error.message}`);
-                    reject(Error(error.message));
-                }
-                if (body){
-                    let results = body;
-                    if(!Array.from(body)){
-                        results = [body];
-                    }
-                    resolve(results);
-                }
-            });
-        })
-    }
+	Query: {
+		launches: async (root, args, { logger, request }) =>
+			new Promise((resolve, reject) => {
+				request(
+					{
+						uri: 'https://api.spacexdata.com/v3/launches',
+						strictSSL: false,
+						json: true
+					},
+					(error, response, body) => {
+						if (error) {
+							logger.error(`Error retrieving data ${error.message}`);
+							reject(Error(error.message));
+						}
+						if (body) {
+							let results = body;
+							if (!Array.from(body)) {
+								results = [ body ];
+							}
+							resolve(results);
+						}
+					}
+				);
+			}),
+		launch: async (root, { flight_number }, { logger, request }) =>
+			new Promise((resolve, reject) => {
+				request(
+					{
+						uri: `https://api.spacexdata.com/v3/launches/${flight_number}`,
+						strictSSL: false,
+						json: true
+					},
+					(error, response, body) => {
+						if (error) {
+							logger.error(`Error retrieving data ${error.message}`);
+							reject(Error(error.message));
+						}
+						if (body) {
+							let results = body;
+							if (!Array.from(body)) {
+								results = [ body ];
+							}
+							resolve(results);
+						}
+					}
+				);
+			})
+	}
 };
