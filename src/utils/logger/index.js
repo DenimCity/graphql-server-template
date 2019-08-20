27883d7d7189e8a0705e3379c51c60e1628e9ddc
jxa8hst <jean_altidor@homdepot.com>
2019-08-20 08:53:30 -0400

    logger exported

--- src/utils/logger/index.js
+++ src/utils/logger/index.js
@@ -1,19 +1,17 @@
-import { configure, getLogger } from 'log4js';
+import { createLogger, format, transports } from 'winston';
+import dayjs from 'dayjs';
 
-configure({
-    appenders: {
-        dashboard: {
-            type: 'console',
-        },
-    },
-    categories: {
-        default: {
-            appenders: ['dashboard'],
-            level: process.env.LOG_LEVEL || 'trace',
-        },
-    },
+const { combine, timestamp, label, printf } = format;
+
+const myFormat = printf(({ level, message, label, timestamp }) => {
+	timestamp = dayjs(timestamp).format('MM-DD-YY HH:mm:ss');
+	return `${timestamp} [${label}] ${level}: ${message}`;
 });
 
-const logger = getLogger('SpaceX App');
+const logger = createLogger({
+	level: 'info',
+	format: combine(label({ label: 'LOGGER' }), timestamp(), myFormat),
+	transports: [ new transports.Console({ level: 'silly' }) ]
+});
 
 export default logger;
